a54c00285105a504b6a5108c12d619645ed50315
JC Nicolas <mrjcnicolas@gmail.com>
2019-01-29 11:13:42 +0000

    jcn feature

---
+++ js/components/jcn/vm.js
@@ -0,0 +1,13 @@
+import template from '../../templates/jcn.html'
+import classes from './computed-classes'
+export default {
+    name: "jcn",
+    template,
+    props: [],
+    data() {
+        return {}
+    },
+    computed: {
+        ...classes
+    }
+}
